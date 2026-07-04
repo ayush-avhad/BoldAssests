fac3159f296f80f699e0fea03ef82f42ed19f15c
Ayush Avhad <ayushavhad25@gmail.com>
2026-07-04 15:18:21 +0530

    Add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['images.unsplash.com'],
+    formats: ['image/avif', 'image/webp'],
+  },
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
